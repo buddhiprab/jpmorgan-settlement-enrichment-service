62a2efd92d0a7d4d5d73d758fe0143c6865cd429
buddhi <buddhi.1986@gmail.com>
2020-05-15 21:17:50 +0800

    schema

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,32 @@
+create schema if not exists ses;
+
+create table if not exists ses.sis_data
+(
+    id                  int            auto_increment primary key,
+    ssi_code            varchar2(20)   not null,
+    payer_acc_num       varchar2(20)   not null,
+    payer_bank          varchar2(50)   not null,
+    receiver_acc_num    varchar2(20)   not null,
+    receiver_bank       varchar2(50)   not null,
+    info                varchar2(50)   not null,
+    creation_time       timestamp      default systimestamp
+);
+
+create table if not exists ses.trade_request
+(
+    id                  number          auto_increment primary key,
+    trade_id            varchar2(20)    not null,
+    ssi_code            varchar2(20)    not null,
+    amount              number(18,2)    not null,
+    currency            varchar2(3)     not null,
+    value_date          varchar2(50)    not null,
+    creation_time       timestamp       default systimestamp
+);
+
+create table if not exists ses.settlement_message
+(
+    id                  number          auto_increment primary key,
+    message_id          varchar2(50)    not null,
+    message             text            not null,
+    creation_time       timestamp       default systimestamp
+);
