ed24e9ed2a8a5edd5576bb7b98977e99b07182f9
buddhi <buddhi.1986@gmail.com>
2020-05-15 20:51:57 +0800

    first

---
+++ src/main/java/com/jpmorgan/ses/BuddhiJpmorganSesApplication.java
@@ -0,0 +1,13 @@
+package com.jpmorgan.ses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuddhiJpmorganSesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuddhiJpmorganSesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jpmorgan/ses/BuddhiJpmorganSesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpmorgan.ses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuddhiJpmorganSesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
