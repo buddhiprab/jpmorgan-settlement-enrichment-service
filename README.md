6553cd468746d87d0c0ed9a9dbb1bafb6d2b15f9
buddhi <buddhi.1986@gmail.com>
2020-05-17 17:44:51 +0800

    tests

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# buddhi-jpmorgan-ses-api
+## Settlements Enrichment Service
+
+#### How to run the program.
+
+#### How to run all the unit tests.
+$ mvn test
+
+
+#### API end points
+
+#####1. Create a new market settlement message
+
+> **Accepts:** Trade Id, SSI Code, Amount, Currency, Value Date
+>
+>**Produces:** Market Settlement Message
+
+##### example 
+
+ request:
+
+```json
+{
+	"tradeId":"16846548",
+	"ssiCode":"OCBC_DBS_2",
+	"amount":12894.65,
+	"currency":"USD",
+	"valueDate":"20022020"
+}
+```
+
+response:
+```json
+
+```
+
+#####2. Fetch an existing market settlement message
+
+>**Accepts:** Trade ID
+>
+>**Produces:** Existing Market Settlement Message
+
+####  Special cases handling
+
+1. 
--- src/test/java/com/jpmorgan/ses/SettlementMessageControllerValidationsTests.java
+++ src/test/java/com/jpmorgan/ses/SettlementMessageControllerValidationsTests.java
@@ -1,18 +1,27 @@
 package com.jpmorgan.ses;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jpmorgan.ses.dto.TradeRequestDto;
 import com.jpmorgan.ses.enums.ErrorMessage;
+import com.jpmorgan.ses.models.SsiData;
+import com.jpmorgan.ses.models.TradeRequest;
+import com.jpmorgan.ses.repositories.SsiDataRepository;
+import com.jpmorgan.ses.repositories.TradeRequestRepository;
 import com.jpmorgan.ses.services.SettlementMessageService;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.beans.BeanUtils.copyProperties;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -25,12 +34,31 @@ class SettlementMessageControllerValidationsTests {
 
     @Autowired
     private MockMvc mockMvc;
+    @MockBean
+    SsiDataRepository ssiDataRepository;
+    @MockBean
+    TradeRequestRepository tradeRequestRepository;
 
-    @Autowired
-    private SettlementMessageService settlementMessageService;
+    private static SsiData ssiData;
+    private static TradeRequestDto tradeRequestDto;
 
     @BeforeAll
     private static void init() {
+        ssiData = new SsiData();
+        ssiData.setSsiCode("OCBC_DBS_1");
+        ssiData.setPayerAccNum("438421");
+        ssiData.setPayerBank("OCBCSGSGXXX");
+        ssiData.setReceiverAccNum("05461368");
+        ssiData.setReceiverBank("DBSSGB2LXXX");
+        ssiData.setInfo("BNF:FFC-4697132");
+
+        tradeRequestDto = TradeRequestDto.builder()
+                .tradeId("16846548")
+                .ssiCode("OCBC_DBS_1")
+                .amount("12894.65")
+                .currency("USD")
+                .valueDate("20022020")
+                .build();
 
     }
 
@@ -49,7 +77,27 @@ class SettlementMessageControllerValidationsTests {
     }
 
     @Test
+    public void create_marketSettlementMessage_tradeRequestId_alreadyExists_validation_400() throws Exception {
+        when(ssiDataRepository.findBySsiCode(any(String.class))).thenReturn(ssiData);
+        TradeRequest tradeRequestDb = new TradeRequest();
+        copyProperties(tradeRequestDto, tradeRequestDb);
+        when(tradeRequestRepository.findByTradeId(any(String.class))).thenReturn(tradeRequestDb);
+
+        String tradeRequest = "{\"tradeId\":\"16846548\", \"ssiCode\":\"OCBC_DBS_2\", \"amount\":12894.65, \"currency\":\"USD\", \"valueDate\":\"20022020\"}";
+
+        mockMvc.perform(post("/market/settlement_message/create")
+                .content(tradeRequest)
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.errorCode").value(ErrorMessage.TRADE_ID_NOT_ALREADY_EXISTS.getCode()))
+                .andExpect(jsonPath("$.errorMessage").value(ErrorMessage.TRADE_ID_NOT_ALREADY_EXISTS.getMessage()));
+    }
+
+    @Test
     public void create_marketSettlementMessage_mandatory_validations_400() throws Exception {
+        when(ssiDataRepository.findBySsiCode(any(String.class))).thenReturn(ssiData);
+        when(tradeRequestRepository.findByTradeId(any(String.class))).thenReturn(null);
 
         String tradeRequest = "{\"ssiCode\":\"OCBC_DBS_1\"}";
 
